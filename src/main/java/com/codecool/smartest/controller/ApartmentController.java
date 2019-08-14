fa8009062880b23bb432e918efd917b968b8e694
Michal Kalinowski <subaru.kali@gmail.com>
2019-08-14 10:10:05 +0200

    added thymeleaf, web config, dashboard schema

--- src/main/java/com/codecool/smartest/controller/ApartmentController.java
+++ src/main/java/com/codecool/smartest/controller/ApartmentController.java
@@ -1,4 +1,13 @@
 package com.codecool.smartest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ApartmentController {
+
+    @GetMapping("/")
+    public String apartmentPage() {
+        return "home";
+    }
 }
