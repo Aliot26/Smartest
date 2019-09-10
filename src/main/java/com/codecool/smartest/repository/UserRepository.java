4948e6502ccf9bab2bb338b9df5c3a963ff71069
Aliot26 <olgakochanowa@gmail.com>
2019-09-10 18:01:55 +0200

    created class UserRepository

---
+++ src/main/java/com/codecool/smartest/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.codecool.smartest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository {
+}
