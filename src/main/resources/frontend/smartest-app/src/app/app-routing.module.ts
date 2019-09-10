8fa522b3770d84fb6b1e864b05dc0e75e250a63e
Aliot26 <olgakochanowa@gmail.com>
2019-09-10 18:55:23 +0200

    created service for retrieving all users

--- src/main/resources/frontend/smartest-app/src/app/app-routing.module.ts
+++ src/main/resources/frontend/smartest-app/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {ApartmentComponent} from "./apartment/apartment.component";
 import {NgModule} from "@angular/core";
 import {ApartmentDetailsComponent} from "./apartment-details/apartment-details.component";
+import {UserComponent} from "./user/user.component";
 
 const routes: Routes = [
   {path: '', component: ApartmentComponent},
   {path: 'apartments', component: ApartmentComponent},
   {path: 'apartment/:id', component: ApartmentDetailsComponent},
+  {path: 'users', component: UserComponent},
 ];
 
 @NgModule({
---
+++ src/main/resources/frontend/smartest-app/src/app/service/user-data.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../user/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDataService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  public retrieveAllUsers(){
+    return this.http.get<User[]>(`http://localhost:8080/users`);
+  }
+
+}
