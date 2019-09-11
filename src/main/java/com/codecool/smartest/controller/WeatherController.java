d65ee0ce05eda52d7c29eb156a41e2aa87ea559e
Aliot26 <olgakochanowa@gmail.com>
2019-09-11 20:52:56 +0200

    added show forecast by coordinates

--- src/main/java/com/codecool/smartest/controller/WeatherController.java
+++ src/main/java/com/codecool/smartest/controller/WeatherController.java
@@ -3,6 +3,7 @@ package com.codecool.smartest.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,14 +14,18 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 public class WeatherController {
 
-    String url = "https://api.darksky.net/forecast/f0b3ed063ce518080be896ce5d4f9111/37.8267,-122.4233";
+    String url = "https://api.darksky.net/forecast/f0b3ed063ce518080be896ce5d4f9111/";
     public HttpURLConnection connection = null;
     RestTemplate restTemplate = new RestTemplate();
 
 
-
-    @GetMapping("/weather")
-    public ResponseEntity<String> getWeather() {
-        return restTemplate.getForEntity(url, String.class);
+    @GetMapping("/weather/{lat}/{lng}")
+    public ResponseEntity<String> getWeather(
+            @PathVariable String lat,
+            @PathVariable String lng
+    ) {
+        String uriNew = url + lat + "," + lng;
+        System.out.println(uriNew);
+        return restTemplate.getForEntity(uriNew, String.class, lat, lng);
     }
 }
--- src/main/resources/frontend/smartest-app/src/app/service/weather.service.ts
+++ src/main/resources/frontend/smartest-app/src/app/service/weather.service.ts
@@ -1,24 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import {Observable} from "rxjs";
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 
 @Injectable()
 export class WeatherService {
 
-  readonly ROOT_URL = 'https://api.darksky.net/forecast/f0b3ed063ce518080be896ce5d4f9111/37.8267,-122.4233';
+  constructor(private http: HttpClient) {
+  }
 
-  constructor(private http: HttpClient) { }
-
-
-  currentForecast(lat: number, lng: number): Observable<any> {
-    let params = new HttpParams();
-    params = params.set('lat', lat.toString() );
-    params = params.set('lng', lng.toString() );
-
-    return this.http.get(this.ROOT_URL,
-      // { params }
-      )
+  public currentForecast(lat, lng){
+    return this.http.get(`http://localhost:8080/weather/${lat}/${lng}`);
   }
 
 }
