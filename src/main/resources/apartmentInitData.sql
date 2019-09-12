4d3c56e48293555129690dbeafd8414cbfdae993
Michal Kalinowski <subaru.kali@gmail.com>
2019-09-12 20:15:54 +0200

    added initial sample data

--- src/main/resources/apartmentInitData.sql
+++
@@ -1,5 +0,0 @@
-TRUNCATE table apartments cascade;
-ALTER SEQUENCE apartments_ap_id_seq RESTART WITH 1;
-INSERT into apartments(number)
-VALUES
-(101), (102), (103), (104), (105), (106), (107), (108), (109), (110), (111), (112), (113), (114), (115), (116), (117), (118), (119);
