5d619749ea41d7b0472704134492e3343dd09948
Michal Kalinowski <subaru.kali@gmail.com>
2019-09-12 20:37:05 +0200

    deleted file

--- src/main/resources/frontend/smartest-app/src/app/usersInitData.sql
+++
@@ -1,5 +0,0 @@
-TRUNCATE table users cascade;
-ALTER SEQUENCE users_user_id_seq RESTART WITH 1;
-INSERT into users(username)
-VALUES
-(101), (102), (103), (104), (105), (106), (107), (108), (109), (110), (111), (112), (113), (114), (115), (116), (117), (118), (119);
