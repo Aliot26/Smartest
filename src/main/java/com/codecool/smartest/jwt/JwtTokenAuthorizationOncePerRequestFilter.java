19841442e13b6e353de29d452a21a9cb49cb72d2
KALIWWA <subaru.kali@gmail.com>
2019-09-25 20:44:55 +0200

    updated UserDetailsService to use jwtDatabaseUserDetailsService

--- src/main/java/com/codecool/smartest/jwt/JwtTokenAuthorizationOncePerRequestFilter.java
+++ src/main/java/com/codecool/smartest/jwt/JwtTokenAuthorizationOncePerRequestFilter.java
@@ -25,7 +25,7 @@ public class JwtTokenAuthorizationOncePerRequestFilter extends OncePerRequestFil
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    private UserDetailsService jwtInMemoryUserDetailsService;
+    private UserDetailsService jwtDatabaseUserDetailsService;
 
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
@@ -58,7 +58,7 @@ public class JwtTokenAuthorizationOncePerRequestFilter extends OncePerRequestFil
         logger.debug("JWT_TOKEN_USERNAME_VALUE '{}'", username);
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
-            UserDetails userDetails = this.jwtInMemoryUserDetailsService.loadUserByUsername(username);
+            UserDetails userDetails = this.jwtDatabaseUserDetailsService.loadUserByUsername(username);
 
             if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
