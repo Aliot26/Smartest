75e95b99ecf3f50e4a59b13ffe88aac4cad09a19
KALIWWA <subaru.kali@gmail.com>
2019-09-26 14:53:11 +0200

    added HttpInterceptorBasicAuthService to generate token header to requests

---
+++ src/main/resources/frontend/smartest-app/src/app/service/http-interceptor-auth.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {BasicAuthenticationService} from "./basic-authentication.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpInterceptorBasicAuthService implements HttpInterceptor {
+
+  constructor(
+    private basicAuthenticationService: BasicAuthenticationService
+  ) {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    const basicAuthHeaderString = this.basicAuthenticationService.getAuthenticatedToken();
+    const username = this.basicAuthenticationService.getAuthenticatedUser();
+
+    if (basicAuthHeaderString && username) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: basicAuthHeaderString
+        }
+      });
+    }
+    return next.handle(request);
+  }
+}
