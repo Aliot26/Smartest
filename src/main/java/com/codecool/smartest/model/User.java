a1266a49fa17ea3a51bb3714f696bbf465751e0e
Aliot26 <olgakochanowa@gmail.com>
2019-10-09 12:37:22 +0200

    added observable to userService

--- src/main/java/com/codecool/smartest/model/User.java
+++ src/main/java/com/codecool/smartest/model/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long userId;
+    private int userId;
 
     @Column
     private String username;
