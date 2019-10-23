5f8676156ada5ab10ab26e621952850dabe9af7c
KALIWWA <subaru.kali@gmail.com>
2019-10-23 14:51:52 +0200

    add exclude

--- server/src/main/java/com/codecool/smartest/SmartestApplication.java
+++ server/src/main/java/com/codecool/smartest/SmartestApplication.java
@@ -2,8 +2,9 @@ package com.codecool.smartest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SmartestApplication {
 
     public static void main(String[] args) {
