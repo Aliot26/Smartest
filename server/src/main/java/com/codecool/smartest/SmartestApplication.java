36c751e8d1749632df1745c0b20c616e208df606
Aliot26 <olgakochanowa@gmail.com>
2019-10-24 09:35:20 +0200

    changed pom.xml

--- server/src/main/java/com/codecool/smartest/SmartestApplication.java
+++ server/src/main/java/com/codecool/smartest/SmartestApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+// (exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class SmartestApplication {
 
     public static void main(String[] args) {
