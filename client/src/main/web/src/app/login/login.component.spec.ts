270889cf095b42688f4452e2e06333900cb4b8bf
Aliot26 <olgakochanowa@gmail.com>
2019-10-24 13:56:07 +0200

    added  some imports4

--- client/src/main/web/src/app/login/login.component.spec.ts
+++ client/src/main/web/src/app/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {LoginComponent} from './login.component';
 import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -11,6 +12,7 @@ describe('LoginComponent', () => {
     TestBed.configureTestingModule({
       declarations: [LoginComponent],
       imports: [
+        RouterModule.forRoot([]),
         FormsModule
       ]
     })
