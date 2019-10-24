a2a8b3862d1c7b7b1500de058992b20cae78778c
Aliot26 <olgakochanowa@gmail.com>
2019-10-24 13:50:46 +0200

    added  some imports3

--- client/src/main/web/src/app/service/basic-authentication.service.spec.ts
+++ client/src/main/web/src/app/service/basic-authentication.service.spec.ts
@@ -1,9 +1,14 @@
 import {TestBed} from '@angular/core/testing';
 
 import {BasicAuthenticationService} from './basic-authentication.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('BasicAuthenticationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule,
+    ]
+  }));
 
   it('should be created', () => {
     const service: BasicAuthenticationService = TestBed.get(BasicAuthenticationService);
