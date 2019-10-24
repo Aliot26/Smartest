4e015166f752ca0bd19ac4dedfad971dd9d68439
Aliot26 <olgakochanowa@gmail.com>
2019-10-24 13:35:09 +0200

    added  imports HttpClientModule

--- client/src/main/web/src/app/service/roles.service.spec.ts
+++ client/src/main/web/src/app/service/roles.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RolesService } from './roles.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('RolesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule,
+    ]
+  }));
 
   it('should be created', () => {
     const service: RolesService = TestBed.get(RolesService);
--- client/src/main/web/src/app/service/user-data.service.spec.ts
+++ client/src/main/web/src/app/service/user-data.service.spec.ts
@@ -1,9 +1,14 @@
 import {TestBed} from '@angular/core/testing';
 
 import {UserDataService} from './user-data.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('UserDataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule,
+    ]
+  }));
 
   it('should be created', () => {
     const service: UserDataService = TestBed.get(UserDataService);
