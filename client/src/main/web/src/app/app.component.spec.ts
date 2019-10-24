90220b02c97fb457088f6e0a368790793b1f0e30
Aliot26 <olgakochanowa@gmail.com>
2019-10-24 14:23:43 +0200

    added  some imports11

--- client/src/main/web/src/app/app.component.spec.ts
+++ client/src/main/web/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import {async, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      declarations: [AppComponent],
+      imports: [ RouterTestingModule ]
     }).compileComponents();
   }));
 
