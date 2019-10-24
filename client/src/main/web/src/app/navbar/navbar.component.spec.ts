38139ffb76a0d2e6328ad513f51671987322e743
Aliot26 <olgakochanowa@gmail.com>
2019-10-24 13:59:40 +0200

    added  some imports5

--- client/src/main/web/src/app/navbar/navbar.component.spec.ts
+++ client/src/main/web/src/app/navbar/navbar.component.spec.ts
@@ -1,6 +1,7 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {NavbarComponent} from './navbar.component';
+import {LoginComponent} from "../login/login.component";
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,7 +9,7 @@ describe('NavbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [NavbarComponent]
+      declarations: [NavbarComponent, LoginComponent]
     })
       .compileComponents();
   }));
