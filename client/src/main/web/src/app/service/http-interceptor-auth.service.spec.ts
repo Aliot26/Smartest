c5120b3374742226e55da9413a3822d8267bee45
Aliot26 <olgakochanowa@gmail.com>
2019-10-24 13:50:15 +0200

    added  some imports2

--- client/src/main/web/src/app/service/http-interceptor-auth.service.spec.ts
+++ client/src/main/web/src/app/service/http-interceptor-auth.service.spec.ts
@@ -1,9 +1,14 @@
 import {TestBed} from '@angular/core/testing';
 
 import {HttpInterceptorAuthService} from './http-interceptor-auth.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('HttpInterceptorAuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule,
+    ]
+  }));
 
   it('should be created', () => {
     const service: HttpInterceptorAuthService = TestBed.get(HttpInterceptorAuthService);
