6d30735212b5ad600602cb87947b12131f9ed8ef
Aliot26 <olgakochanowa@gmail.com>
2019-10-24 13:23:39 +0200

    imports HttpClientModule

--- client/src/main/web/src/app/service/apartment-data.service.spec.ts
+++ client/src/main/web/src/app/service/apartment-data.service.spec.ts
@@ -1,9 +1,14 @@
 import {TestBed} from '@angular/core/testing';
 
 import {ApartmentDataService} from './apartment-data.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('ApartmentDataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule,
+    ],
+  }));
 
   it('should be created', () => {
     const service: ApartmentDataService = TestBed.get(ApartmentDataService);
