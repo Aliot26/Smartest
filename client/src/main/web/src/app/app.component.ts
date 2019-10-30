edeefb65260781d7d48de30b153e38135adc9404
Aliot26 <olgakochanowa@gmail.com>
2019-10-30 12:13:23 +0100

    added  some imports29

--- client/src/main/web/src/app/app.component.ts
+++ client/src/main/web/src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'smartest-app';
+  title = 'SmartestApp';
 
   ngOnInit() {
 
