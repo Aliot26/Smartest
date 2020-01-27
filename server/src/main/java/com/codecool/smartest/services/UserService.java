a9e84e9f624f7e18be062d9d92c396c1d717e38b
Aliot26 <olgakochanowa@gmail.com>
2020-01-27 14:44:35 +0100

    Added user service interface and implementation

---
+++ server/src/main/java/com/codecool/smartest/services/UserService.java
@@ -0,0 +1,12 @@
+package com.codecool.smartest.services;
+
+import com.codecool.smartest.model.User;
+
+import java.util.List;
+
+/*
+ *Created by olga on 27.01.20
+ */
+public interface UserService {
+    List<User> getUsers();
+}
---
+++ server/src/main/java/com/codecool/smartest/services/impl/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.codecool.smartest.services.impl;
+
+import com.codecool.smartest.model.User;
+import com.codecool.smartest.repository.UserRepository;
+import com.codecool.smartest.services.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ *Created by olga on 27.01.20
+ */
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        List<User> userList = new ArrayList<>();
+        userRepository.findAll().iterator().forEachRemaining(userList::add);
+        return  userList;
+    }
+}
